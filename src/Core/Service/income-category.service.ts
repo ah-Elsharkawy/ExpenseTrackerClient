1045f180225420be6accf7ebf71bd48c4f0247cb
EmadAhmedNasef <emadnasef86@gmail.com>
2024-06-14 19:06:33 +0300

    changed api-url

--- src/Core/Service/income-category.service.ts
+++ src/Core/Service/income-category.service.ts
@@ -3,18 +3,21 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Category } from '../Interface/category';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IncomeCategoryService {
   private categories: Category[] = [];
+  private apiUrl = environment.apiUrl;
+
   categoryId: BehaviorSubject<number> = new BehaviorSubject(-1);
 
   constructor(private _HttpClient: HttpClient) {}
 
   getCategories(): Observable<any> {
-    return this._HttpClient.get("https://localhost:44311/api/services/app/Category/GetCategories").pipe(
+    return this._HttpClient.get(`${this.apiUrl}/services/app/Category/GetCategories`).pipe(
       map((response: any) => {
         this.categories = response.result;
         return response;
