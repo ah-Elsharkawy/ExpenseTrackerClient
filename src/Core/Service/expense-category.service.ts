eba3b372359f31eb47c3ba31863307c63a5fb887
ahmed-fahmy17 <fahomahmed@gmail.com>
2024-06-15 20:27:43 +0300

    adding transactions and recurrence in expense

--- src/Core/Service/expense-category.service.ts
+++ src/Core/Service/expense-category.service.ts
@@ -1,51 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Category } from '../Interface/category';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenseCategoryService {
+  private categories: Category[] = [];
+  private apiUrl = environment.apiUrl;
 
-  constructor() {}
+  categoryId: BehaviorSubject<number> = new BehaviorSubject(-1);
 
-  categories: Category[] = [ 
-    {icon:'attach_money', name:'Salary', id:1},
-    {icon:'card_giftcard', name:'Gifts', id:2},
-    {icon:'business', name:'Business', id:3},
-    { icon: 'restaurant', name: 'Food & Drink' ,id:4},
-    { icon: 'shopping_cart', name: 'Shopping' ,id:5},
-    { icon: 'directions_bus', name: 'Transport' ,id:6 },
-    { icon: 'receipt', name: 'Bills & Fees' ,id:7 },
-    { icon: 'theaters', name: 'Entertainment',id:8 },
-    { icon: 'directions_car', name: 'Car' ,id:9 },
-    { icon: 'flight', name: 'Travel' ,id:10 },
-    { icon: 'people', name: 'Family' ,id:11 },
-    { icon: 'local_hospital', name: 'Healthcare' ,id:12 },
-  ];
+  constructor(private _HttpClient: HttpClient) {}
 
-  expenseCategories: Category[] = [ 
-    {icon:'money_off', name:'Bills', id:1},
-    {icon:'card_giftcard', name:'Gifts', id:2},
-    {icon:'account_balance', name:'Business', id:3},
-    {icon: 'fastfood', name:'Food & Drink', id:4},
-    {icon: 'shopping_basket', name:'Shopping', id:5},
-    {icon: 'commute', name:'Transport', id:6},
-    {icon: 'receipt_long', name:'Bills & Fees', id:7},
-    {icon: 'local_movies', name:'Entertainment', id:8},
-    {icon: 'directions_car', name:'Car', id:9},
-    {icon: 'flight_takeoff', name:'Travel', id:10},
-    {icon: 'group', name:'Family', id:11},
-    {icon: 'local_pharmacy', name:'Healthcare', id:12},
-];
-
-  categoryId : BehaviorSubject<number> = new BehaviorSubject(-1);
-  
-  getCategoryNameById(id: number): string {
-    return this.categories.find((category) => category.id === id)?.name || '';
+  getCategories(): Observable<any> {
+    return this._HttpClient.get(`${this.apiUrl}/services/app/Category/GetCategoriesByType?type=1`).pipe(
+      map((response: any) => {
+        this.categories = response.result;
+        return response;
+      })
+    );
   }
 
   getCategoryId(): number | null {
     return this.categoryId.getValue();
+  }
+
+  isCategoryChoosen(): boolean {
+    return this.getCategoryId() == -1;
+  }
+
+  getCategoryNameById(id: number): string | null{
+    const category = this.categories.find(category => category.id === id);
+    return category ? category.name : null;
   }
 }
