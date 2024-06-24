77f42cd21b3e926a2207c95a359e9955f0b4a6b9
EmadAhmedNasef <emadnasef86@gmail.com>
2024-06-24 22:16:49 +0300

    fixed

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { InsightsComponent } from '../insights/insights.component';
 import { IncomeDTComponent } from '../income-dt/income-dt.component';
-import { ExpenseDTComponent } from '../expense-dt/expense-dt.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [InsightsComponent,IncomeDTComponent,ExpenseDTComponent],
+  imports: [InsightsComponent,IncomeDTComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
