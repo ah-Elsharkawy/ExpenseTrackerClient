fd6c2656f5b95c13cdff01154522fb94646d6a30
EmadAhmedNasef <emadnasef86@gmail.com>
2024-07-01 21:33:36 +0300

    first cards static

--- src/app/Components/insights/line-chart/line-chart.component.ts
+++ src/app/Components/insights/line-chart/line-chart.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-line-chart',
   standalone: true,
-  imports: [ChartModule],
+  imports: [ChartModule, MatCardModule],
   templateUrl: './line-chart.component.html',
   styleUrl: './line-chart.component.css'
 })
